a73a42d4dbdbf53a58f2c4459b5ee9cce177f586
John <johnckane1@gmail.com>
2020-07-29 21:11:51 -0500

    finish creating draft data through 2019 season

--- data/bn-draft-data/format-draft-data.R
+++ data/bn-draft-data/format-draft-data.R
@@ -89,11 +89,11 @@ draft2019$position_value_keeper <-
     2
   )
 
-draft2019$position <- 
+draft2019$pos <- 
   str_extract(draft2019$position_value_keeper,'^ [A-Z//]+ ') %>%
   str_remove_all(.," ")
-with(draft2019,table(position))
-draft2019$value <- 
+with(draft2019,table(pos))
+draft2019$value2 <- 
   str_remove(
     str_extract(draft2019$position_value_keeper,'\\$\\d{1,3}'),
     '\\$'
@@ -115,7 +115,7 @@ draft2019$team <- toupper(draft2019$team)
 
 draft2019 <-
   draft2019 %>%
-  select(year,owner,pick,player,team,position,value,keeper)
+  select(year,owner,pick,player,team,pos,value2,keeper)
 
 draft_data <- bind_rows(draft_data,draft2019)
 
@@ -128,25 +128,43 @@ draft_data <- bind_rows(draft_data,draft2019)
 draft_data$player <- str_replace(draft_data$player, "[*]","")
 
 # Change Le'Veon Bell
-draft_data$player <- ifelse(draft_data$player == "Le Veon Bell", "Le'Veon Bell",draft_data$player)
+draft_data$player <- 
+  ifelse(
+    draft_data$player == "Le Veon Bell", 
+    "Le'Veon Bell",
+    draft_data$player
+    )
 
 # Make Steve Smith Steve Smith Sr.
-draft_data$player <- ifelse(draft_data$player == "Steve Smith" & draft_data$team %in% c("BAL","CAR"),
-                            "Steve Smith Sr.",
-                            draft_data$player)
+draft_data$player <- 
+  ifelse(
+    draft_data$player == "Steve Smith" & draft_data$team %in% c("BAL","CAR"),
+    "Steve Smith Sr.",
+    draft_data$player
+  )
 
-draft_data$player <- ifelse(draft_data$pos == "HC",str_replace(draft_data$player,"HC",""),draft_data$player)
+draft_data$player <- 
+  ifelse(
+    draft_data$pos == "HC",
+    str_replace(draft_data$player,"HC",""),
+    draft_data$player)
+
 draft_data$player <- str_trim(draft_data$player)
 
 #' Look at all D/ST, see if we need to change those names
 #' 
 draft_data %>% filter(pos == "D/ST")
 
+draft_data <- draft_data
+
 draft_data$first_name <-  sapply(str_split(draft_data$player,' '), '[[', 1)      
 draft_data$last_name <- sapply(str_split(draft_data$player, ' '), '[[', 2)
+
+
 head(draft_data)
 
-draft_data <- draft_data %>%
+draft_data <- 
+  draft_data %>%
   mutate(player = ifelse(pos == "D/ST",
                          paste(first_name,"D/ST",sep = " "),
                          player))
@@ -154,13 +172,21 @@ draft_data <- draft_data %>%
 
 draft_data$player <- str_trim(draft_data$player)
 
-draft_data$player <- ifelse(draft_data$player == "Steve Smith" & draft_data$team %in% c("BAL","CAR"),
-                            "Steve Smith Sr.",
-                            draft_data$player)
+draft_data$player <- 
+  ifelse(
+    draft_data$player == "Steve Smith" & draft_data$team %in% c("BAL","CAR"),
+    "Steve Smith Sr.",
+    draft_data$player
+  )
 
-draft_data_players <- draft_data %>% group_by(player, first_name, last_name) %>% slice(1) %>% select(player, first_name, last_name)
+draft_data_players <- 
+  draft_data %>% 
+  group_by(player, first_name, last_name) %>% 
+  slice(1) %>% 
+  select(player, first_name, last_name)
 
-draft_data <- draft_data %>%
+draft_data <- 
+  draft_data %>%
   arrange(player,year) %>%
   group_by(player) %>%
   mutate(player_obs = row_number())
@@ -187,10 +213,15 @@ draft_data %>% filter(value2 != adj_value)
 #' ) Unique players in the draft data
 #' 2) Their player_key
 
-draft_data_players <- draft_data %>% group_by(player, first_name, last_name) %>% slice(1) %>% select(player, first_name, last_name)
+draft_data_players <- 
+  draft_data %>% 
+  group_by(player, first_name, last_name) %>% 
+  slice(1) %>% 
+  select(player, first_name, last_name)
 
 
 ## Connect to the database
+library(pool)
 pool <- dbPool(drv = RMySQL::MySQL(), 
                dbname = "armchair_analysis", 
                host = "localhost", 
@@ -229,48 +260,51 @@ uncoded <-
 
 uncoded %<>%
   mutate(player_code = ifelse(player == 'Adrian Peterson', 'AP-0700',
-                              ifelse(player == 'Alex Smith','AS-1600',
-                                     ifelse(player == 'Antonio Brown','AB-3500',
-                                            ifelse(player == 'Beanie Wells','CW-1400',
-                                                   ifelse(player == 'Brandon Marshall','BM-0300',
-                                                          ifelse(player == 'Cam Newton','CN-0500',
-                                                                 ifelse(player == 'Charles Johnson','CJ-1450',
-                                                                        ifelse(player == 'Chris Givens', 'CG-2060', player_code ))))))))) %>%
+                       ifelse(player == 'Alex Smith','AS-1600',
+                       ifelse(player == 'Antonio Brown','AB-3500',
+                       ifelse(player == 'Beanie Wells','CW-1400',
+                       ifelse(player == 'Brandon Marshall','BM-0300',
+                       ifelse(player == 'Cam Newton','CN-0500',
+                       ifelse(player == 'Charles Johnson','CJ-1450',
+                       ifelse(player == 'Chris Givens', 'CG-2060', player_code ))))))))) %>%
   mutate(player_code = ifelse(player == "Chris Johnson",'CJ-1700',
-                              ifelse(player == 'David Johnson','DJ-1325',
-                                     ifelse(player == 'Derek Dimke',NA,
-                                            ifelse(player == 'EJ Manuel','EM-0250',
-                                                   ifelse(player == 'James Jones','JJ-4200',
-                                                          ifelse(player == 'Jonathan Stewart','JS-6700',
-                                                                 ifelse(player == 'Kevin Smith','KS-1700',
-                                                                        ifelse(player %in% c('Marvin Jones','Marvin Jones Jr.'), 'MJ-2250',
-                                                                               ifelse(player == 'Mike Sims-Walker', 'MW-0400', player_code)))))))))) %>%
+                       ifelse(player == 'David Johnson','DJ-1325',
+                       ifelse(player == 'Derek Dimke',NA,
+                       ifelse(player == 'EJ Manuel','EM-0250',
+                       ifelse(player == 'James Jones','JJ-4200',
+                       ifelse(player == 'Jonathan Stewart','JS-6700',
+                       ifelse(player == 'Kevin Smith','KS-1700',
+                       ifelse(player %in% c('Marvin Jones','Marvin Jones Jr.'), 'MJ-2250',
+                       ifelse(player == 'Mike Sims-Walker', 'MW-0400', player_code)))))))))) %>%
   mutate(player_code = ifelse(player == 'Mike Williams' & team == 'SEA','MW-2700',
-                              ifelse(player == 'Mike Williams' & team %in% c('BUF','TB'), 'MW-2800',
-                                     ifelse(player == 'Odell Beckham Jr.','OB-0075',
-                                            ifelse(player == 'Ricky Williams','RW-2600',
-                                                   ifelse(player == 'Royce Adams',NA,
-                                                          ifelse(player == 'Roy Williams','RW-3200',
-                                                                 ifelse(player == 'Ryan Grant','RG-1600',
-                                                                        ifelse(player == 'Steve Smith Sr.','SS-2100',
-                                                                               ifelse(player == 'Steve Smith', 'SS-2200', player_code )))))))))) %>%
+                       ifelse(player == 'Mike Williams' & team %in% c('BUF','TB'), 'MW-2800',
+                       ifelse(player == 'Odell Beckham Jr.','OB-0075',
+                       ifelse(player == 'Ricky Williams','RW-2600',
+                       ifelse(player == 'Royce Adams',NA,
+                       ifelse(player == 'Roy Williams','RW-3200',
+                       ifelse(player == 'Ryan Grant','RG-1600',
+                       ifelse(player == 'Steve Smith Sr.','SS-2100',
+                       ifelse(player == 'Steve Smith', 'SS-2200', player_code )))))))))) %>%
   mutate(player_code = ifelse(player == 'T.Y. Hilton','TH-1850',
-                              ifelse(player == 'Zach Miller','ZM-0200',player_code))) %>%
+                       ifelse(player == 'Zach Miller','ZM-0200',player_code))) %>%
   mutate(player_code = ifelse(player == 'Kevin White' & year %in% c(2016,2017), 'KW-0887',
-                              ifelse(player == 'Matt Jones' & year == 2016,'MJ-2275',
-                                     ifelse(player == 'Michael Thomas','MT-0875',
-                                            ifelse(player == 'Michael Vick','MV-0300',
-                                                   ifelse(player == 'Chad Ochocinco','CJ-1200',
-                                                                 ifelse(player == 'Wil Lutz','WL-0300',
-                                                                        ifelse(player == 'Chris Thompson','CT-1220',player_code)))))))) %>%
+                       ifelse(player == 'Matt Jones' & year == 2016,'MJ-2275',
+                       ifelse(player == 'Michael Thomas','MT-0875',
+                       ifelse(player == 'Michael Vick','MV-0300',
+                       ifelse(player == 'Chad Ochocinco','CJ-1200',
+                       ifelse(player == 'Wil Lutz','WL-0300',
+                       ifelse(player == 'Chris Thompson','CT-1220',player_code)))))))) %>%
   mutate(player_code = ifelse(player == 'Mike Thomas','MT-0900',
-                              ifelse(player == 'Steven Hauschka','SH-0400',
-                                     ifelse(player == 'JuJu Smith-Schuster','JS-4750',
-                                            ifelse(player == 'DJ Moore','DM-2850',
-                                                   ifelse(player == 'Josh Allen','JA-1087',player_code))))))
+                       ifelse(player == 'Steven Hauschka','SH-0400',
+                       ifelse(player == 'JuJu Smith-Schuster','JS-4750',
+                       ifelse(player == 'DJ Moore','DM-2850',
+                       ifelse(player == 'Josh Allen','JA-1087',
+                       ifelse(player == 'Mike Williams' & team == 'LAC','MW-2825',
+                       ifelse(player == 'Greg Little', 'GL-0400',
+                       ifelse(player == 'DK Metcalf', 'DM-2275',player_code)))))))))
 
 
-uncoded %>% filter(is.na(player_code) == TRUE)
+uncoded %>% filter(is.na(player_code) == TRUE) %>% View
 
 # Cannot match Derek Dimke or Royce Adams
 
@@ -290,6 +324,19 @@ dbGetQuery(conn = pool,
            statement = "SELECT * FROM player WHERE lname = 'Moore'")
 dbGetQuery(conn = pool,
            statement = "SELECT * FROM player WHERE lname = 'Allen'")
+dbGetQuery(conn = pool,
+           statement = "SELECT * 
+                        FROM player 
+                        WHERE fname = 'Mike' and lname = 'Williams'
+                          AND pos1 = 'WR'")
+dbGetQuery(conn = pool,
+           statement = "SELECT * 
+                        FROM player 
+                        WHERE fname = 'Greg' and lname = 'Little'")
+dbGetQuery(conn = pool,
+           statement = "SELECT * 
+                        FROM player 
+                        WHERE lname = 'Metcalf'")
 
 
 
@@ -300,7 +347,7 @@ coded_draft_data <-
 lapply(coded_draft_data, function(x) sum(is.na(x)))
 
 coded_draft_data %>%
-  filter(is.na(team) == TRUE) %>%
+   filter(is.na(team) == TRUE) %>%
   print(n = 90)
 
 
@@ -328,8 +375,8 @@ coded_draft_data %<>%
 
 coded_draft_data %<>%
   filter((player == 'Rams D/ST' & year %in% c(2013,2014) & abbr == 'LA') == FALSE,
-         (player == 'Rams D/ST' & year %in% c(2015,2016,2018) & abbr == 'STL') == FALSE,
-         (player == 'Rams Coach' & year == 2018 & abbr == 'STL') == FALSE)
+         (player == 'Rams D/ST' & year %in% c(2015,2016,2018,2019) & abbr == 'STL') == FALSE,
+         (player == 'Rams Coach' & year %in% c(2018,2019) & abbr == 'STL') == FALSE)
 
 coded_draft_data %>% group_by(player,year) %>% summarise(count = n()) %>% filter(count > 1)
 
